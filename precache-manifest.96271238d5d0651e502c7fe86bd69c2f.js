f74ada31206b6d7bbb4f3753232f562490f3aa9c
unknown <ahmed.hadjsaad@iit.ens.tn>
2020-09-25 11:13:50 +0200

    Updates

---
+++ precache-manifest.96271238d5d0651e502c7fe86bd69c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5f119f3c40dfde31dfa4486befd5e4",
+    "url": "/checkpoint-props.git/index.html"
+  },
+  {
+    "revision": "b43137706ec30f71714a",
+    "url": "/checkpoint-props.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a40fb25eff7f92ebcbf",
+    "url": "/checkpoint-props.git/static/js/2.cd713e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/checkpoint-props.git/static/js/2.cd713e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43137706ec30f71714a",
+    "url": "/checkpoint-props.git/static/js/main.bea0b97a.chunk.js"
+  },
+  {
+    "revision": "333d2f7e37acdcc03935",
+    "url": "/checkpoint-props.git/static/js/runtime-main.2b614ae7.js"
+  }
+]);
